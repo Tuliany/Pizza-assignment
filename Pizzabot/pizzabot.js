5b3e3b974fc6efa2ef936fdfaf29448aded49d54
Tuliany <tuliany@mail.com>
2020-10-30 11:42:28 +0100

    added som HTML

--- Pizzabot/pizzabot.js
+++ Pizzabot/pizzabot.js
@@ -1,4 +1,45 @@
-const vegetarian = "Vegetarian Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
+
+const pizzaPrice = 80;
+
+let pizzaChoise = '';
+let pizzaNum;
+
+
+function checkOrderName(theOrderName) {
+  return theOrderName == vegetarian || theOrderName == hawaiian || theOrderName == pepperoni;
+}
+
+function totalCost(theOrderQuantity) {
+  return theOrderQuantity * pizzaPrice;
+}
+
+function calculateCookingTime(theOrderQuantity) {
+  if (theOrderQuantity > 0 && theOrderQuantity <= 2) {
+    return 10;
+  } else if (theOrderQuantity > 2 && theOrderQuantity <= 5) {
+    return 15;
+  } else if (theOrderQuantity > 5) {
+    return 20;
+  }
+}
+
+function doOrder() {
+  pizzaChoise = document.querySelector('#pizza').value;
+  pizzaNum = document.querySelector('#numPizzas').value;
+  const cost = totalCost(pizzaNum);
+  const cookingTime = calculateCookingTime(pizzaNum);
+  console.log(cost, cookingTime);
+
+  const resultElement = document.querySelector('#orderResult');
+  resultElement.textContent = ` Great, I'll get started on your ${pizzaChoise} right away, it will cost ${cost} kr and it will take ${cookingTime} minutes`;
+}
+
+document.querySelector('#orderButton').addEventListener('click', doOrder);
+
+/* const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80;
@@ -40,6 +81,6 @@ alert(`Great, I'll get started on your ${orderName} right away, it will cost ${o
 
 
 
-
+ */
 
 
